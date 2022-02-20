a299ccd9dc39bcb7d4845c65fd8b70bedd6a2370
dillingspam <dillingspam@gmail.com>
2022-02-20 15:03:22 -0500

    Removed no details error page

--- app/src/main/java/com/gitds/amazonsmilehelper/MainActivity.java
+++ app/src/main/java/com/gitds/amazonsmilehelper/MainActivity.java
@@ -33,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        WebView.enableSlowWholeDocumentDraw();
         setContentView(R.layout.activity_main);
 
         Objects.requireNonNull(getSupportActionBar()).hide();
